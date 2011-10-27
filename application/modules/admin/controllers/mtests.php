4b880307132ed318304f4179a8f31e86c7972e48
miladh <milad.b.patterson@gmail.com>
2011-10-27 19:45:51 +0330

    added admin module, with simple tests.

---
+++ application/modules/admin/controllers/mtests.php
@@ -0,0 +1,19 @@
+<?php
+
+class Mtests extends MY_Controller {
+	function testGetSellersWithSingleSeller () {
+		$this->db->query('truncate sellers;');
+		$this->db->insert('sellers', array(
+			'username' => 'milad',
+			'password' => 'milad',
+			'user_type' => 'seller',
+			'email' => 'miladbashiri@comp.iust.ac.ir',
+			'creation_time' => date('m-d-Y H:i:s'),
+			'display_name' => 'milad'
+		));
+		$this->load->model('seller_model');
+		$data = $this->seller_model->get_seller_names();
+		$this->assertEqual(count($data), 1);
+		$this->assertEqual($data[0]['display_name'], 'milad');
+	}
+}
---
+++ application/modules/admin/controllers/vtests.php
@@ -0,0 +1,5 @@
+<?php
+
+class Vtests extends WebTestCase {
+	
+}
