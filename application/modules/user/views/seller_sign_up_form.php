513b556000f97c8ba6b3371e4ee52e1b2da33800
sadegh kazemy <sadegh.kazemy@gmail.com>
2011-11-14 22:03:31 +0330

    extra comments removed from login_form and sign_up_form

--- application/modules/user/views/seller_sign_up_form.php
+++ application/modules/user/views/seller_sign_up_form.php
@@ -30,42 +30,7 @@
 		<input id="reg_btm" type="submit" value="ثبت‌نام" name="submit">
 	</form>
 	<?php
-	//$attributes = array('class' => 'forms');
-	//echo form_open('user/login/register', $attributes);
-
-	//echo form_input('username', set_value('username', 'نام کاربری'), 'id="username" class="check"');
-	//echo '<div class="error_msg" id="username_error"> </div>';
-
-	//echo form_input('password', 'رمز عبور', 'id="password" class="check pass"');
-	//echo '<div id="password_error"> </div>';
-	//echo form_input('passconf', 'تکرار رمز عبور', 'id="c_password"  class="check pass"');
-	//echo '<div id="c_password_error"> </div>';
-
-	//echo form_input('first_name', set_value('first_name', 'نام'), 'id="first_name" class="check"');
-	//echo '<div class="error_msg" id="first_name_error"> </div>';
-
-	//echo form_input('last_name', set_value('last_name', 'نام خانوادگی'), 'id="last_name" class="check"');
-	//echo '<div class="error_msg" id="last_name_error"> </div>';
-
-	//echo form_input('email', set_value('email', 'آدرس پست‌الکترونیکی'), 'id="email"  class="check"');
-	//echo '<div id="email_error"> </div>';
-
-	//echo form_input('phone', 'شماره‌ی تماس', 'id="phone" class="check"');
-	//echo '<div id="phone_error"> </div>';
-
-	//echo form_input('address', 'آدرس دقیق', 'id="address" class="check"');
-	//echo '<div id="address_error"> </div>';
-
-	//echo form_input('seller_display_name', 'نام شرکت یا فروشگاه شما', 'id="seller_display_name" class="check"');
-	//echo '<div id="address_error"> </div>';
-
-	//echo form_hidden('ut', 's');
-
-	//echo '<div id="register_msg">' . 'لطفاً پیش از ثبت‌نام قوانین سایت را مطالعه بفرمایید.' . '<br/>' . anchor('site/rules', 'قوانین سایت', 'id="register_btm"') . '<br/> <br/>' . form_checkbox('confirmation', '', FALSE, 'class="confirmation"') . '<label for="confirmation">قوانین سایت را مطالعه کرده و قبول می‌کنم</label>' . '</div>';
-
-	//echo form_submit('submit', 'ثبت‌نام', 'id="reg_btm"');
-
-	//echo form_close();
+	
 	?>
 </div>
 {/block}
