66d4414f3522fb382d48f35f3992247fbb333a63
Hamed Gholizadeh <hamed.gholizadeh.f@gmail.com>
2011-11-16 11:06:53 +0330

    email change

--- application/config/email.php
+++ application/config/email.php
@@ -3,6 +3,6 @@
     $config['protocol'] = 'smtp';
     $config['smtp_host'] = 'ssl://smtp.googlemail.com';
     $config['smtp_port'] = 465; //can use 587 too
-    $config['smtp_user'] = 'serahi@gmail.com'; //user name
-    $config['smtp_pass'] = 'pass'; //pass word
+    $config['smtp_user'] = 'serahi90@gmail.com'; //user name
+    $config['smtp_pass'] = 'miladserahi'; //pass word
 ?>
--- application/modules/admin/controllers/email.php
+++ application/modules/admin/controllers/email.php
@@ -4,16 +4,16 @@
 class email extends MY_Controller {
 
     function index() {
-        $email_to = 'hamed.gholizadeh.f@gmail.com';
-        $email_subj = 'This is an email';
-        $email_text = 'It is working. Great!';
+        $email_to = 'serahi90@gmail.com';
+        $email_subj = 'Email From Serahi';
+        $email_text = 'In Yek Email Ast!';
         $this->send($email_to, $email_subj, $email_text);
     }
 
     function send($email_to, $email_subj, $email_text) {
         $this->load->library('email');
         $this->email->set_newline("\r\n");
-        $this->email->from('serahi@gmail.com', 'Serahi');
+        $this->email->from('serahi90@gmail.com', 'info');
         $this->email->to($email_to);
         $this->email->subject($email_subj);
         $this->email->message($email_text);
