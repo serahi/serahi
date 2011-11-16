24ab617f10f68451a9a3c4b17e2b50be1a9758d7
Hamed Gholizadeh <hamed.gholizadeh.f@gmail.com>
2011-11-16 12:02:15 +0330

    final email library

---
+++ application/libraries/Email_agent.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Email_agent{
+
+    function __construct() {
+    }
+    function send($email_to, $email_subj, $email_text) {
+        $CI =& get_instance();
+        $CI->load->library('email');
+        $CI->email->set_newline("\r\n");
+        $CI->email->from('serahi90@gmail.com', 'info');
+        $CI->email->to($email_to);
+        $CI->email->subject($email_subj);
+        $CI->email->message($email_text);
+
+        if ($CI->email->send()) {
+            return true;
+        } else {
+            return show_error($CI->email->print_debugger());
+        }
+    }
+}
--- application/modules/admin/controllers/email.php
+++ application/modules/admin/controllers/email.php
@@ -1,28 +1,15 @@
 <?php
 
 // sends email with gmail
-class email extends MY_Controller {
+class Email extends MY_Controller {
 
     function index() {
+
+        $this->load->library('Email_agent');
         $email_to = 'serahi90@gmail.com';
         $email_subj = 'Email From Serahi';
         $email_text = 'In Yek Email Ast!';
-        $this->send($email_to, $email_subj, $email_text);
-    }
-
-    function send($email_to, $email_subj, $email_text) {
-        $this->load->library('email');
-        $this->email->set_newline("\r\n");
-        $this->email->from('serahi90@gmail.com', 'info');
-        $this->email->to($email_to);
-        $this->email->subject($email_subj);
-        $this->email->message($email_text);
-
-        if ($this->email->send()) {
-            echo 'email sent';
-        } else {
-            show_error($this->email->print_debugger());
-        }
+        $this->email_agent->send($email_to, $email_subj, $email_text);
     }
 
 }
