a2b11b7917d3bfaa5bf66f87645da1f325ffa342
Hamed Gholizadeh <hamed.gholizadeh.f@gmail.com>
2011-11-16 10:04:23 +0330

    email part

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    $config['protocol'] = 'smtp';
+    $config['smtp_host'] = 'ssl://smtp.googlemail.com';
+    $config['smtp_port'] = 465; //can use 587 too
+    $config['smtp_user'] = 'serahi@gmail.com'; //user name
+    $config['smtp_pass'] = 'pass'; //pass word
+?>
