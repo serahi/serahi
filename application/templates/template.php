fad7bfe54bdda4174f8b43715731a1eda8365e27
miladh <milad.b.patterson@gmail.com>
2011-11-17 23:56:36 +0330

    add missing js.

--- application/templates/template.php
+++ application/templates/template.php
@@ -16,6 +16,7 @@
 		<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/jquery.validate.js"></script>
 		<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/jquery.validationEngine.js"></script>
 		<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/jquery.validationEngine-en.js"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/jquery.easy-confirm-dialog.js"></script>
 		<script type="text/javascript" src="<?php echo base_url();?>assets/scripts/my_script.js"></script>
 		<script type="text/javascript">
 		
