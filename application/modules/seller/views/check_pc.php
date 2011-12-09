380eadddc2f6f69a26ce1e5384c7fbe2783140d6
sadegh <sadeq@wout.(none)>
2011-12-09 21:49:04 +0330

    check_pc view's recoverd

---
+++ application/modules/seller/views/check_pc.php
@@ -0,0 +1,27 @@
+{block name=title}
+بررسی کد رهگیری
+{/block}
+{block name=main_content}
+
+<div class="g_form" >
+<form action="<?php echo base_url()?>seller/check_pursuit_code" method="post" class="forms submit_form">
+    
+        <?php
+        if(isset($not_found_pc))
+        {
+            echo '<div class="error_msg">' . $not_found_pc['msg'] . '</div>';
+        }
+        if (isset($found_pc))
+        {
+            echo '<div class="ok_msg">' . $found_pc['msg'] . '</div>';
+        }
+        
+        ?>
+    
+    <input type="text" class="check validate[required,minSize[10],maxSize[10]" name="pursuit_code" value="کد رهگیری">
+    <input type="submit" class="submit_btm"  name="submit" value="بررسی کن!" >
+</form>
+    
+</div>
+
+{/block}
--- non-code stuff/sql scripts/restore.sh
+++ non-code stuff/sql scripts/restore.sh
@@ -1,2 +1,2 @@
-/opt/PostgreSQL/9.1/bin/psql -U postgres -d serahi < clear-db.sql
-/opt/PostgreSQL/9.1/bin/psql -U postgres -d serahi < serahi.sql
+psql -U postgres -d serahi < clear-db.sql
+psql -U postgres -d serahi < serahi.sql
