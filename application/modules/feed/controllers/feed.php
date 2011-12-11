162be0e761195a657c9c1d08a3bceb93e902c1c0
sadegh <sadeq@wout.(none)>
2011-12-12 01:57:53 +0330

    RSS module added

---
+++ application/modules/feed/controllers/feed.php
@@ -0,0 +1,20 @@
+<?php
+
+class Feed extends MY_Controller{
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('xml');
+        $this->load->helper('text');
+        $this->load->model('posts_model');
+    }
+    
+    function index(){
+        $data['posts'] = $this->posts_model->get_posts(10);
+        $this->load->view('feed', $data);
+    }
+    
+    
+    
+    
+}
---
+++ application/modules/feed/models/posts_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class Posts_model extends CI_Model{
+    function get_posts( $limit = NULL ){
+                
+        $this->db->order_by('date', "desc");
+        return $this->db->get('posts_rss', $limit );
+    }
+}
