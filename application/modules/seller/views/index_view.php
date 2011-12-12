bc02813f3c20b627b74e660a0e2313de7d192f2e
miladh <milad.b.patterson@gmail.com>
2011-12-12 13:39:38 +0330

    add number of delivered transactions in seller product view.

--- application/modules/seller/views/index_view.php
+++ application/modules/seller/views/index_view.php
@@ -28,8 +28,13 @@
 			<div class="sell_count">
 				<b> تعداد سفارش شده: </b>{$product.sell_count}
 			</div>
+			{if $product.delivered_count > 0}
+			<div class="delivered_count">
+				<b> تعداد تحویل شده: </b>{$product.delivered_count}
+			</div>
+			{/if}
 			<div class = "timeline_container">
-				نمودار میزان فروش محصولات بر حسب زمان:
+				نمودار تعداد سفارش محصول بر حسب زمان:
 				<span class = "timeline_graph" values = '{$product.timeline}'></span>
 			</div>
 		</div>
