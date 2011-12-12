6a8aaf24dfd66bbfbef8813b111ef71b38d9187e
Hessam Mohammadian <hessam.mohammadian@gmail.com>
2011-12-12 15:46:25 +0330

    + news style fixed

--- application/modules/home/views/news.php
+++ application/modules/home/views/news.php
@@ -3,8 +3,10 @@
 <div class="product">
      <?php foreach ($news as $new): ?>
      <div class="item">
-         <div class="item_title"> <?php echo $new['title'];?> </div>
-         <div class="description"> <?php echo $new['content']; ?> </div>
+         <div class="item_title"> <b> <?php echo $new['title'];?> </b> </div>
+         <div class="news_date"> <?php echo $new['date'];?> </div>
+         <div class="desc"> <?php echo $new['content']; ?> </div>
+        
     </div>
      <?php endforeach;?>
 </div>
