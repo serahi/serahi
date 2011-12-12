0805a63a65f57467ca248c8207e07d9ea9c343a7
miladh <milad.b.patterson@gmail.com>
2011-12-12 10:07:46 +0330

    add sparkline library for drawing graphs using jquery. add timeline for number of transactions per hour of day.

--- application/modules/seller/models/seller_model.php
+++ application/modules/seller/models/seller_model.php
@@ -11,14 +11,43 @@ class Seller_model extends CI_Model
 			$this->db->where('product_id', $product->id);
 			$this->db->from('transactions');
 			$count = $this->db->count_all_results();
+			$timeline_query = $this->db->query('select extract(hour from transaction_time) * 2 + '.
+			                                          'floor(extract(minute from transaction_time) / 30) AS time_id, '.
+			                                          'count(*) from transactions '.
+												                 'where product_id = ? ' . 
+												                 'group by time_id '.
+																				 'order by time_id;', array($product->id));
+			$timeline_array = $timeline_query->result_array();
+			//echo '<pre>'; print_r($timeline_array); echo '</pre>'; //die;
+			$timeline = array();
+			for ($i=0; $i < count($timeline_array); $i++) {
+				$timeline[$timeline_array[$i]['time_id']] = $timeline_array[$i]['count'];
+			}
+			//echo '<pre>'; print_r($timeline); echo '</pre>'; die;
+			$timeline_str = '';
+			for ($i=0; $i < 48; $i++) {
+				if (!isset($timeline[$i])) {
+					$timeline_str .= '0';
+				} else {
+					$timeline_str .= $timeline[$i];
+				}
+				if ($i != 47) $timeline_str .= ',';
+			}
 			$results[] = array(
 					'product_name' => $product->product_name,
 					'lower_limit' => $product->lower_limit,
-					'sell_count' => $count
+					'sell_count' => $count,
+					'timeline' => $timeline_str
 			);
 		}
 		return $results;
 	}
+
+/* select extract(hour from transaction_time) * 2 + 
+         floor(extract(minute from transaction_time) / 30) AS time_id,
+         count(*) from transactions
+group by time_id;
+ */
 
 	function check_pursuit_code ()
 	{
